cefadd1a69ea3affbad26f32ec4adaaa9d619b3d
JoeChester <jzone123@googlemail.com>
2016-12-01 21:52:21 +0100

    added settings buttons on main screen

--- src/www/js/main.js
+++ src/www/js/main.js
@@ -1,5 +1,16 @@
 var width;
 var height;
+var ip;
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 function jqUpdateSize(){
     // Get the dimensions of the viewport
@@ -15,10 +26,21 @@ function jqUpdateSize(){
 $(document).ready(jqUpdateSize);    
 $(window).resize(jqUpdateSize);    
 
+ip = getParameterByName("ip");
+console.log(ip);
+
 // Camera Live Preview
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         video.src = window.URL.createObjectURL(stream);
         video.play();
     });
+}
+
+function photo(){
+    console.log("PHOTO!");
+}
+
+function settings(){
+    window.location = "/?ip=" + ip; 
 }
--- src/www/js/settings.js
+++ src/www/js/settings.js
@@ -1,6 +1,12 @@
-$(function(){
-    $("#ip").focus();
-});
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
 function ok(){
     var ip = $("#ip").val();
@@ -12,3 +18,8 @@ function ok(){
     console.log(ip);
     window.location = "cam.html?ip=" + ip;
 }
+
+$(function(){
+    $("#ip").val(getParameterByName("ip") || "localhost");
+    $("#ip").focus();
+});
