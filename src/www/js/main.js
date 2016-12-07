ab92d99fce36bb3edd654b23abc633aaffb1d26f
JoeChester <jzone123@googlemail.com>
2016-12-07 12:00:36 +0100

    added snapshot layering v1

--- src/www/js/main.js
+++ src/www/js/main.js
@@ -2,6 +2,12 @@ var width;
 var height;
 var ip;
 
+var video = document.getElementById('video');
+var photo = document.getElementById('photo');
+var x3dElem = document.getElementById('x3dom');
+var x3dCanvas;
+var ctx = photo.getContext('2d');
+
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
@@ -17,17 +23,65 @@ function jqUpdateSize(){
     width = $(window).width();
     height = $(window).height();
 
-    $('#x3dom').attr('height', height);      
-    $('#x3dom').attr('width', width);
+    $('.fullscreen').attr('height', height);      
+    $('.fullscreen').attr('width', width);       
+}
 
-    $('#video').attr('height', height);      
-    $('#video').attr('width', width);        
-};
+function snapshot(){
+
+    var _videoWidth = video.videoWidth;
+    var _videoHeight = video.videoHeight;
+    var videoRatio = _videoWidth / _videoHeight;
+    console.log(videoRatio);
+
+    var currentHeight = $('#video').innerHeight();
+    console.log(currentHeight);
+
+    var currentWidth = videoRatio * currentHeight;
+    console.log(currentWidth);
+
+    var anchorX = (width / 2) - (currentWidth / 2);
+    console.log(anchorX);
+
+
+    ctx.drawImage(video, anchorX, 0, currentWidth, currentHeight);
+    x3dCanvas = x3dCanvas = document.getElementById('x3dom-x3dom-canvas');
+    ctx.drawImage(x3dCanvas, 0, 0, width, height);
+
+    saveToServer();
+
+}
+
+function settings(){
+    window.location = "/?ip=" + ip; 
+}
+
+function resetWhite(){
+  ctx.beginPath();
+  ctx.rect(0, 0, width, height);
+  ctx.fillStyle = "white";
+  ctx.fill();
+}
+
+// POST to PHP saver script (ul.php)
+function saveToServer(){
+  var dataURL = photo.toDataURL();
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost/ul.php",
+    data: {
+      imgBase64: dataURL
+    }
+  }).done(function(o) {
+    console.log('saved');
+  });
+}
+
 $(document).ready(jqUpdateSize);    
-$(window).resize(jqUpdateSize);    
+$(window).resize(jqUpdateSize);  
 
 ip = getParameterByName("ip");
-console.log(ip);
 
 // Camera Live Preview
 if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -35,12 +89,4 @@ if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         video.src = window.URL.createObjectURL(stream);
         video.play();
     });
-}
-
-function photo(){
-    console.log("PHOTO!");
-}
-
-function settings(){
-    window.location = "/?ip=" + ip; 
 }
